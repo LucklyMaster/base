dd77b4f591f7e84f9d49f8ef79e4f11dd6e21f78
MasterChan <chenyang0529>
2022-08-19 11:37:26 +0800

    添加正则

--- baseLib/src/main/java/com/master/lib/ext/String.kt
+++ baseLib/src/main/java/com/master/lib/ext/String.kt
@@ -45,3 +45,50 @@ fun Context.getStringList(@StringRes vararg resources: Int): List<String> {
 fun Context.getStringArray(@StringRes vararg resources: Int): Array<String> {
     return getStringList(*resources).toTypedArray()
 }
+
+/**
+ * 判断是否是手机号码
+ * @receiver String
+ * @return Boolean
+ */
+fun String.isPhoneNum(): Boolean {
+    return "^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}\$".toRegex()
+        .matches(this)
+}
+
+/**
+ * 判断是否是邮箱
+ * @receiver String
+ * @return Boolean
+ */
+fun String.isEmail(): Boolean {
+    return "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*\$".toRegex().matches(this)
+}
+
+/**
+ * 判断是否是域名
+ * @receiver String
+ * @return Boolean
+ */
+fun String.isDomain(): Boolean {
+    return "[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\\.?".toRegex()
+        .matches(this)
+}
+
+/**
+ * 判断是否是URL地址
+ * @receiver String
+ * @return Boolean
+ */
+fun String.isInternetUrl(): Boolean {
+    return "[a-zA-z]+://[^\\s]*".toRegex().matches(this)
+}
+
+/**
+ * 判断是否是身份证号
+ * @receiver String
+ * @return Boolean
+ */
+fun String.isIdCardNum(): Boolean {
+    return "(^\\d{15}\$)|(^\\d{18}\$)|(^\\d{17}(\\d|X|x)\$)".toRegex().matches(this)
+}
